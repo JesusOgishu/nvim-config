c66aa8f367799873983e4190bb9ec1451b9f38fc
jesusOgishu <alvarez1campos@gmail.com>
2026-03-02 20:50:34 -0600

    block cursor

--- init.lua
+++ init.lua
@@ -8,14 +8,14 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--branch=stable",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -28,3 +28,13 @@ require("vim-options")
 
 -- Keymaps
 require("config.keymaps")
+-- Desactivar completamente el mouse
+vim.opt.mouse = ""
+
+-- Ocultar el cursor en todos los modos
+vim.opt.guicursor = ""
+-- Deshabilitar scroll accidental
+vim.keymap.set("", "<ScrollWheelUp>", "<Nop>")
+vim.keymap.set("", "<ScrollWheelDown>", "<Nop>")
+vim.keymap.set("", "<ScrollWheelLeft>", "<Nop>")
+vim.keymap.set("", "<ScrollWheelRight>", "<Nop>")
